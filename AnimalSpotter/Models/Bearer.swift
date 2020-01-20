719df5b51b26d4d04c789b7b9f10d716011a2c0d
David Wright <davidtwright89@gmail.com>
2020-01-20 02:28:21 -0500

    Completed guided project

---
+++ AnimalSpotter/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  AnimalSpotter
+//
+//  Created by David Wright on 1/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
---
+++ AnimalSpotter/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  AnimalSpotter
+//
+//  Created by David Wright on 1/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let username: String
+    let password: String
+}
